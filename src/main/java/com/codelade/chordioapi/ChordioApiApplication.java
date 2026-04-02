fd43e388ab0d7be1b9946b622a1b2de350896215
Codelade <j.klimiuk1999@gmail.com>
2026-04-02 01:33:18 +0100

    Basic set up of the project with all dependencies as well as database configuration.

---
+++ src/main/java/com/codelade/chordioapi/ChordioApiApplication.java
@@ -0,0 +1,13 @@
+package com.codelade.chordioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChordioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChordioApiApplication.class, args);
+    }
+
+}
