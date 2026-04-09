3870ffbf6a9dfdd6fa00114d3682d2e85f7a5136
Codelade <j.klimiuk1999@gmail.com>
2026-04-09 01:04:00 +0100

    Add authentication DTOs for login and registration, including validation constraints

---
+++ src/main/java/com/codelade/chordioapi/dto/auth/RegisterRequestDto.java
@@ -0,0 +1,26 @@
+package com.codelade.chordioapi.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequestDto {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String userName;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message ="Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+}
